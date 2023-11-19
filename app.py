018c5e49652ed339ecc88eda3f256de8e6367f98
erichwang111 <142135666+erichwang111@users.noreply.github.com>
2023-11-19 12:40:51 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from pythonfiles.main_project import text_preprocessing
-
+from interactive_dashboard import create_dashboard
+from dash import Dash
+import dash_bootstrap_components as dbc
 
 app = Flask(__name__)
 app.secret_key = "abcd"
 ADMIN_ID = "Administrator"
 PASSWORD = "123"
-session = {"username":ADMIN_ID,}
+session = {"username":ADMIN_ID}
+dash_app = Dash(server=app, external_stylesheets=[dbc.themes.BOOTSTRAP], routes_pathname_prefix="/dashboard/")
 
+dash_app = create_dashboard(dash_app)
 
 @app.route("/",methods=['GET'])
 def index():
@@ -15,6 +18,10 @@ def index():
     if "username" in session:
         isLogin = True
     return render_template("index.html",isLogin=isLogin)
+
+@app.route("/dashboard/",methods=['GET'])
+def dashboard():
+    pass
 
 @app.route("/login",methods=['GET','POST'])
 def login():
@@ -24,7 +31,7 @@ def login():
 
         if password == PASSWORD and username == ADMIN_ID:
             session["username"] = ADMIN_ID
-            return redirect(url_for('index'))
+            return redirect(url_for('dashboard'))
         else:
             flash("Password or Admin ID is incorrect")
             return render_template("login.html")
